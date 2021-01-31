532468d2333eb436cdaf017941111188d16279a3
Moacir <moacir9401@gmail.com>
2021-01-31 16:46:47 -0300

    evitando foreingn key null

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -19,5 +19,12 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList();
         }
 
+        public void Insert(Seller obj)
+        {
+            obj.Department = _context.Department.First();
+            _context.Seller.Add(obj);
+            _context.SaveChanges();
+        }
+
     }
 }
